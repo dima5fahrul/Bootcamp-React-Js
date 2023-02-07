ff35e3b4b8fdb39b7c2bfa3c38171c3b96c932ed
dima5fahrul <dfpa.05@gmail.com>
2023-02-07 19:51:23 +0700

    Pengumpulan Tugas 7

---
+++ tugas-reactjs/src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="container">
+          <h1>Hi, Saya John</h1>
+          <hr />
+          <p>Saya dari batch 42 sudah berhasil menginstall React!</p>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
